03a4dafd0b44d6d5123d09618fa1c9e9828a3cd4
Heitor Tashiro Sergent <heitortsergent@gmail.com>
2015-07-14 11:21:56 -0500

    Redirect to signup when login fails

--- routes/index.js
+++ routes/index.js
@@ -96,7 +96,7 @@ module.exports = function(passport) {
 
     router.post('/login', passport.authenticate('local', {
         successRedirect: '/',
-        failureRedirect: '/',
+        failureRedirect: '/signup',
         failureFlash: true
     }));
 
